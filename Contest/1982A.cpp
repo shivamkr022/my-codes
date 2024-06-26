53172bc113db8e7b472e8e66a0083afe3e730dc1
shivamkr022 <shivamshinu02@gmail.com>
2024-06-26 20:32:47 +0530

    cp

--- Contest/1982A.cpp
+++ Contest/1982A.cpp
@@ -40,9 +40,9 @@ using namespace std;
 void solve(){
         int x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
-        if ((x1 < y1 && x2 < y2) || (x1 > y1 && x2 > y2)) {
+        if((x1 < y1 && x2 < y2) || (x1 > y1 && x2 > y2)) {
             cout << "YES" << endl;
-        } else {
+        }else {
             cout << "NO" << endl;
         }
 }
