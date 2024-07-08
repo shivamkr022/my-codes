1261b97adc0147b4de580dee2197c4a09730b14e
shivamkr022 <shivamshinu02@gmail.com>
2024-07-08 14:42:51 +0530

    cp

---
+++ codeforces/codeforces/1176A.cpp
@@ -0,0 +1,80 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n;
+    cin>>n;
+		int cnt2 = 0, cnt3 = 0, cnt5 = 0;
+		while(n % 2 == 0){
+			n /= 2;
+			++cnt2;
+		}
+		while(n % 3 == 0){
+			n /= 3;
+			++cnt3;
+		}
+		while(n % 5 == 0){
+			n /= 5;
+			++cnt5;
+		}
+		if(n != 1){
+			cout << -1 << endl;
+		}
+        else{
+			cout << cnt2 + cnt3 * 2 + cnt5 * 3 << endl;
+		}
+
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
