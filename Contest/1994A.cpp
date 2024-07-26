5be6db3ce1f9a1a9206fc460c8e29d24a6c128be
shivamkr022 <shivamshinu02@gmail.com>
2024-07-26 15:46:19 +0530

    cp

---
+++ Contest/1994A.cpp
@@ -0,0 +1,87 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>>a(n,vector<int>(m));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>a[i][j];            // input
+        }
+    }
+
+   if(n==1 and m==1){
+    cout<<-1<<endl;        // cant be changed
+    return;
+   }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(a[i][j]!=n*m){
+                a[i][j]++;      // all ele inc by 1
+            }
+            else{
+                a[i][j]=1; // except n*m
+            }
+        }
+    }
+   
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){        // output
+            cout<<a[i][j]<<" ";
+        }cout<<endl;
+    }
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
--- practice/ss1.cpp
+++ practice/ss1.cpp
@@ -1,107 +1,63 @@
-/*
-  ------------------------------------------
- |                                        |
- |      Code Crafted by Shivam     |
- |                                        |
-  ------------------------------------------
-    \        ,     ,        /
-      \      |     |      /
-         \   \_/   /
-           \  -----  /
-             \_/
-  
-  Happy coding! 
-*/
-
-/* includes and all */
-
-#include<bits/stdc++.h>
-#ifndef ONLINE_JUDGE
-#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
-#define print(v) do { \
-                    cout << "vect--" << #v << " = [ "; \
-                    for (int i = 0; i < v.size(); i++) { \
-                        cout << v[i] << " "; \
-                    } \
-                    cout << " ]" << endl; \
-                } while(0)
-#else
-#define debug(x)
-#define print(v)
-#endif
-#define endl "\n"
-#define int long long int
-#define mod 1000000007
-#define mn(a,b,c) min(a,min(b,c))
-#define mx(a,b,c) max(a,max(b,c))
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-bool cross(int n, int m, int k, const string &a) {   // fn to check it can cross road or not
+int main() {
+    int t;
+    cin >> t;
     
-    vector<bool> rch(n + 2, false);
-    rch[0] = true;  // starting point is reachable always
-
-    queue<pair<int, int>> qu;
-    qu.push({0, 0});  
-
-    while (!qu.empty()) {
-        int pos = qu.front().first;
-        int dist = qu.front().second;
-        qu.pop();
-        for (int i = 1; i<= m; i++) {
-            int pos1 = pos + i;
-            if (pos1 >= n + 1) {
-                return true;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        
+        vector<vector<int>> a(n, vector<int>(m));
+        vector<int> all_elements;
+        
+        // Read matrix a
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                cin >> a[i][j];
+                all_elements.push_back(a[i][j]);
             }
-            if (pos1 <= n && (a[pos1 - 1] == 'C' || rch[pos1])) {
-                continue;
+        }
+        
+        // Sort all elements
+        sort(all_elements.begin(), all_elements.end());
+        
+        // Create matrix b
+        vector<vector<int>> b(n, vector<int>(m));
+        
+        int index = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                b[i][j] = all_elements[(index + 1) % (n * m)];
+                index++;
             }
-            if (pos1 <= n && a[pos1 - 1] == 'L') {
-                rch[pos1] = true;
-                qu.push({pos1, dist});
-            }
-            if (pos1 <= n && a[pos1 - 1] == 'W') {
-                if (dist + 1 <= k) {
-                    rch[pos1] = true;
-                    qu.push({pos1, dist + 1});
+        }
+        
+        // Check if matrix b is valid
+        bool valid = true;
+        for (int i = 0; i < n && valid; ++i) {
+            for (int j = 0; j < m && valid; ++j) {
+                if (a[i][j] == b[i][j]) {
+                    valid = false;
                 }
             }
-            if (pos1 == n + 1) {
-                return true;
+        }
+        
+        // Output result
+        if (valid) {
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < m; ++j) {
+                    cout << b[i][j] << " ";
+                }
+                cout << endl;
             }
+        } else {
+            cout << -1 << endl;
         }
     }
-    return false;
+    
+    return 0;
 }
-
-/* write core logic here */
-void solve(){
-    int n, m, k;
-    cin >> n >> m >> k;
-    string cat;
-    cin >> cat;
-
-    if (cross(n, m, k, cat)) {
-        cout << "YES" << endl;
-    }
-    else{
-       cout << "NO" << endl;
-        }
-    }
-/* logic ends */
-
-signed main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    #ifndef ONLINE_JUDGE
-        freopen("Error.txt" , "w" , stderr);
-    #endif
-    int t;
-    cin>>t;
-   // t = 1;
-    while(t--){
-        solve();
-    }
-return 0;
-}
-
