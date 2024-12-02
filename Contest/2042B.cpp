a1cbf950d8ba1364d9cf2f28432be267b97c7404
shivamkr022 <shivamshinu02@gmail.com>
2024-12-02 22:33:06 +0530

    cp

---
+++ Contest/2042B.cpp
@@ -0,0 +1,86 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by SASUKE    |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    set<int>s;
+    for(int ele :arr){
+       s.insert(ele);
+    }
+      int xx = 0;
+      int duplicate=0;
+    unordered_map<int,int>map1;
+    for(int i=0;i<n;i++){
+        map1[arr[i]]++;
+    }
+    for(auto element :map1){
+        if(element.second<2){
+            xx++;
+        }
+        else{
+            duplicate++;
+        }
+    }
+  
+   // int rem = n-xx;
+    int jydacoins = 2*((xx+1)/2);
+    cout<<(jydacoins+duplicate)<<endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
