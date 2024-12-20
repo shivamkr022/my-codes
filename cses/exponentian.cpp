e5deb45269d09aa0d16be3acc73dbdc49b78be16
shivamkr022 <shivamshinu02@gmail.com>
2024-12-20 15:09:35 +0530

    cp

--- cses/exponentian.cpp
+++ cses/exponentian.cpp
@@ -1,43 +1,53 @@
 /*
-  ------------------------------------------
- |                                        |
- |      Code Crafted by Shivam     |
- |                                        |
-  ------------------------------------------
-    \        ,     ,        /
-      \      |     |      /
-         \   \_/   /
-           \  -----  /
-             \_/
-  
-  Happy coding! 
+**  Compete against Yourself.
+**  Author - Shivam kumar
 */
 
 /* includes and all */
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-#include<climits>
-#define ll long long int
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
 #define mod 1000000007
+int po(int a,int b) {if(b==0) return 1; if(b==1) return a; if(b%2==0) {int u=po(a,b/2);return (u*1LL*u)%mod;} else {int u=po(a,b-1);return (a*1LL*u)%mod;}}
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+#define all(x) (x).begin(),(x).end()
 using namespace std;
-ll f(int a,int b){
-    if(b==0) return 1;
-    ll res = f(a,b/2);
-    if(b%2==1){
-        return (((res*res)%mod)*a)%mod;
+
+/* write core logic here */
+void solve(){
+  int a,b; cin>>a>>b;
+  cout<<po(a,b)<<endl;
+
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
     }
-    else
-    return (res*res)%mod;
+return 0;
 }
 
-int main(){
-    int n;
-    cin>>n;
-    while(n--){
-        int a,b;
-        cin>>a>>b;
-        cout<<f(a,b)<<endl;
-    }
-}
--- practice/ssppp.cpp
+++ practice/ssppp.cpp
@@ -1,16 +1,6 @@
 /*
-  ------------------------------------------
- |                                        |
- |      Code Crafted by Shivam     |
- |                                        |
-  ------------------------------------------
-    \        ,     ,        /
-      \      |     |      /
-         \   \_/   /
-           \  -----  /
-             \_/
-  
-  Happy coding! 
+**  Compete against Yourself.
+**  Author - Shivam kumar
 */
 
 /* includes and all */
@@ -32,31 +22,19 @@
 #define endl "\n"
 #define int long long int
 #define mod 1000000007
+int po(int a,int b) {if(b==0) return 1; if(b==1) return a; if(b%2==0) {int u=po(a,b/2);return (u*1LL*u)%mod;} else {int u=po(a,b-1);return (a*1LL*u)%mod;}}
 #define mn(a,b,c) min(a,min(b,c))
 #define mx(a,b,c) max(a,max(b,c))
+#define all(x) (x).begin(),(x).end()
 using namespace std;
 
 /* write core logic here */
 void solve(){
-    int n;
-    cin>>n;
-    vector<int>arr(n);
-    for(int i=0;i<n;i++) cin>>arr[i];
-    vector<int>arr1=arr;
-
-    int count =2;
-    for(int i=0;i<n;i++){
-        if(arr[i]!=i+1 and count!=0){
-            swap(arr[i],arr[arr[i]-1]);
-            count--;
-        }
-    }
-    sort(arr1.begin(),arr1.end());
-    if(arr==arr1){
-        cout<<"YES"<<endl;
-        return ;
-    }
-    cout<<"NO"<<endl;
+      int a,b;
+      cin>>a>>b;
+      int ans = po(a,b);
+      int final = ans%mod;
+      cout<<final<<endl;
 }
 /* logic ends */
 
@@ -67,8 +45,8 @@ signed main(){
         freopen("Error.txt" , "w" , stderr);
     #endif
     int t;
-    //cin>>t;
-    t = 1;
+    cin>>t;
+    //t = 1;
     while(t--){
         solve();
     }
