abefecf6f9181accb6360cf2bf48542aa2c7c2aa
shivamkr022 <shivamshinu02@gmail.com>
2024-12-25 18:36:25 +0530

    cp

---
+++ codeforces/codeforces/2043B.cpp
@@ -0,0 +1,69 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n,d;
+    cin>>n>>d;
+    vector<int>ans;
+    ans.push_back(1);
+    if(d % 3 == 0 or n >= 3) ans.push_back(3);
+    if(d == 5) ans.push_back(5);
+    if(n >= 3 or d == 7) ans.push_back(7);
+    if(d % 9 == 0 or (d % 3 == 0 and n >= 3) or (n >= 6)) ans.push_back(9);
+    for(int ele:ans){
+        cout<<ele<<" ";
+    }cout<<endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
