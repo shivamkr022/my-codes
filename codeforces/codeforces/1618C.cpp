a46ad63c6f0cb1fa1000893b3c3a00d7c3aca34d
shivamkr022 <shivamshinu02@gmail.com>
2025-01-07 00:08:11 +0530

    cp

---
+++ codeforces/codeforces/1618C.cpp
@@ -0,0 +1,76 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=1;i<=n;i++) cin>>arr[i];
+    vector<int>arr1;
+    vector<int>arr2;
+    bool flag1= true;
+    bool flag2 = true;
+    bool flag3= true;
+    bool flag4 = true;
+    for(int i=1;i<=n;i++){ // odd posn odd ele
+    if(i%2==0){
+        arr1.push_back(arr[i]);
+    }
+    else{
+        arr2.push_back(arr[i]);
+    }
+  }
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    //cin>>t;
+    t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
