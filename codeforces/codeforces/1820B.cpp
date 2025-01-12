862e3eee56a48d3af98a2f20f69ba206cc92b5da
shivamkr022 <shivamshinu02@gmail.com>
2025-01-12 17:15:16 +0530

    cp

---
+++ codeforces/codeforces/1820B.cpp
@@ -0,0 +1,81 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    string s;
+    cin>>s;
+    int ones =0;
+    int tt=0;
+    s+=s;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='1'){
+            tt++;
+            ones=max(tt,ones);
+        }
+        else{
+            tt=0;
+        }
+    }
+    int n = s.size()/2;
+    if(ones>n){
+        cout<<n*n<<endl;
+    }
+    else{
+    int a = (ones+1)/2;
+    int b = (ones+2)/2;
+    cout << a * b << endl;
+    }
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
