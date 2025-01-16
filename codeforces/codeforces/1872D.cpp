811ad97f835fd3f93993bca5b6e58c367251341a
shivamkr022 <shivamshinu02@gmail.com>
2025-01-16 15:26:06 +0530

    cp

---
+++ codeforces/codeforces/1872D.cpp
@@ -0,0 +1,69 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+int smmm(int k){
+    return k*(k+1)/2;
+}
+/* write core logic here */
+void solve(){
+    int n,l,r;
+    cin>>n>>l>>r;
+    int x = n/r;
+    int y = n/l;
+    int lcm =(l*r)/(__gcd(l,r));
+    int cmn = n/lcm;
+    int xr = x-cmn;
+    int yr = y-cmn;
+    cout<<(smmm(n)-smmm(n-yr)-smmm(xr))<<endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
