26f59a21996537e27fe9a49a5c6a9efb03634ab9
shivamkr022 <shivamshinu02@gmail.com>
2025-02-15 11:20:23 +0530

    cp

---
+++ codeforces/codeforces/1433C.cpp
@@ -0,0 +1,85 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    int ans =-1;
+    set<int>s;
+    for(int ele :arr){
+        s.insert(ele);
+    }
+    if(s.size()==1){
+        cout<<-1<<endl;
+        return;
+    }
+    int xx = INT_MIN;
+    for(int i=0;i<n;i++){
+        xx= max(xx,arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]!=xx) continue;
+        if(i>0 and arr[i-1]!=xx){
+            ans =i+1;
+        }
+        if(i<n-1 and arr[i+1]!=xx){
+            ans =i+1;
+        }
+    }
+    cout<<ans<<endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
