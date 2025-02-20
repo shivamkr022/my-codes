b8fd382889c234b2a736a7cd9d9d105c6387dfe6
shivamkr022 <shivamshinu02@gmail.com>
2025-02-20 16:56:24 +0530

    cses

---
+++ cses/towers.cpp
@@ -0,0 +1,88 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    // int n;
+    // cin>>n;
+    // vector<int>arr(n);
+    // for(int i=0;i<n;i++) cin>>arr[i];
+    // int ans = 1;
+    // int mxm = arr[0];
+    // for(int i=1;i<n;i++){
+    //     if(arr[i]>=mxm){
+
+    //         ans++;
+    //         mxm= arr[i];
+    //     }
+    // }
+    // cout<<ans<<endl;
+	int n;
+	cin >> n;
+	multiset<int>ans;
+	for (int i = 0; i < n; ++i) {
+        int k;
+		cin >> k;
+		auto it = ans.upper_bound(k);
+		if (it == ans.end()){
+        ans.insert(k);
+        }
+		else{
+			ans.erase(it);
+			ans.insert(k);
+		}
+	}
+	cout<<ans.size()<<endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    //cin>>t;
+    t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
