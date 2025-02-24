bf6e667d2053a76c5c8b5d8995e0d27ffc682c69
shivamkr022 <shivamshinu02@gmail.com>
2025-02-24 13:29:51 +0530

    cp

---
+++ codeforces/codeforces/479A.cpp
@@ -0,0 +1,74 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int a;
+    cin>>a;
+    int b;
+    cin>>b;
+    int c;
+    cin>>c;
+    int c1 = a*b*c;
+    int c2 = (a*b)+c;
+    int c3 = a+(b*c);
+    int c4 = (a+b)*c;
+    int c5= (a+b+c);
+    int c6 = a*(b+c);
+    int cc1 =mx(c1,c2,c3);
+    int cc2 = mx(c4,c5,c6);
+   // int ans = mx(c1,c2,c3);
+    cout<<max(cc1,cc2)<<endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    //cin>>t;
+    t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
