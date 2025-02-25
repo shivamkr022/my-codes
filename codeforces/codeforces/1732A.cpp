a1a52777dd8a151919dc08daa832cd926ca9efb2
shivamkr022 <shivamshinu02@gmail.com>
2025-02-25 17:11:08 +0530

    cp

---
+++ codeforces/codeforces/1732A.cpp
@@ -0,0 +1,80 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    int gg = arr[0];
+    for(int i = 1; i < n; i++) {
+        gg = __gcd(gg, arr[i]);
+    }
+   // debug(gcd);
+    if(gg==1){
+        cout<<0<<endl;
+        return;
+    }
+    if(__gcd(gg,n)==1){
+        cout<<1<<endl;
+        return;
+    }
+    if(__gcd(gg,n-1)==1){
+        cout<<2<<endl;
+        return;
+    }
+    cout<<3<<endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
