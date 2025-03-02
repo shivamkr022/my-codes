5de210229c39ca23c3614728ff672fc1a2ba16b7
shivamkr022 <shivamshinu02@gmail.com>
2025-03-02 13:14:58 +0530

    cp

---
+++ codeforces/codeforces/1934B.cpp
@@ -0,0 +1,79 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+int f(int n){
+    int ans=0;
+    ans+=n/15;
+    n%=15;
+    ans+=n/6;
+    n%=6;
+    ans+=n/3;
+    n%=3;
+    ans+=n;
+    return ans;
+}
+/* write core logic here */
+void solve(){
+    int n;
+    cin>>n;
+    if(n<10){
+        cout<<f(n)<<endl;
+    }
+    else if(n<20){
+        cout<<min(f(n),f(n-10)+1)<<endl;
+    }
+    else{
+        cout<<min({f(n),f(n-10)+1,f(n-20)+2})<<endl;
+    }
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
