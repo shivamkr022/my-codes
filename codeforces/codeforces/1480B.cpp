f45864a2dcc94d98285eaf44a41a290e89dce954
shivamkr022 <shivamshinu02@gmail.com>
2025-03-21 14:58:14 +0530

    cp

---
+++ codeforces/codeforces/1480B.cpp
@@ -0,0 +1,69 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+#define ceilDiv(x, y) ((x + y - 1) / y)
+/* write core logic here */
+void solve(){
+    int A,B,n;
+    cin>>A>>B>>n;
+    vector<int>a(n),b(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    for(int i=0;i<n;i++) cin>>b[i];
+    int maxA = 0;
+    for (int i = 0; i < n; ++i) {
+        B -= a[i] * ceilDiv(b[i], A);
+        maxA = max(maxA, a[i]);
+    }
+    cout << (B + maxA > 0 ? "YES" : "NO") << endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
