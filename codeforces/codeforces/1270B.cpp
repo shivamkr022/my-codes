0517e95e6ab7ff0dc8c364814aa2a59040d1321d
shivamkr022 <shivamshinu02@gmail.com>
2025-03-24 13:25:57 +0530

    cp

---
+++ codeforces/codeforces/1270B.cpp
@@ -0,0 +1,82 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int l=0;
+    int r=0;
+    for(int i=1;i<n;i++){
+        if(abs(arr[i]-arr[i-1])>1){
+            l=i-1;
+            r=i;
+            break;
+        }
+    }
+  //  debug(arr[l]);
+  //  debug(arr[r]);
+    if(l+r>0){
+        cout<<"YES"<<endl;
+        cout<<(l+1)<<" "<<(r+1)<<" "<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
