259c1f1400731620bdb90ac4283d9414c5978a9e
shivamkr022 <shivamshinu02@gmail.com>
2025-03-30 12:34:11 +0530

    cp

---
+++ codeforces/codeforces/2092C.cpp
@@ -0,0 +1,92 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    int ans=0;
+    sort(arr.begin(),arr.end());
+    bool flag = true;
+    for(int ele :arr){
+        if(ele%2==0){
+            flag = false;
+            break;
+        }
+    }
+    int xx = *max_element(arr.begin(),arr.end());
+    if(flag){
+        cout<<xx<<endl;
+        return;
+    }
+    int sum =0;
+    int cnt =0;
+    for(int ele:arr){
+        if(ele%2==1){
+            cnt++;
+        }
+    }
+    int eve = n-cnt;
+    if(eve==n){
+        cout<<xx<<endl;
+        return;
+    }
+    for(int ele :arr){
+        sum+=ele;
+    }
+    cout<<(sum-cnt+1)<<endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
