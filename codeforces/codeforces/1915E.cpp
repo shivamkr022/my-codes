45d3e8e82b4d47e51a9da0ab5b4922efa03b0452
shivamkr022 <shivamshinu02@gmail.com>
2025-04-01 15:00:13 +0530

    cp

---
+++ codeforces/codeforces/1915E.cpp
@@ -0,0 +1,86 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        if(i%2==1){
+            arr[i]=-(arr[i]);
+        }
+    }
+    vector<int>pre(n);
+    pre[0]=arr[0];
+    for(int i=1;i<n;i++){
+        pre[i]=arr[i]+pre[i-1];
+    }
+    sort(pre.begin(),pre.end());
+    bool flag = false;
+    for(int i=1;i<n;i++){
+        if(pre[i]==pre[i-1] or pre[i-1]==0){
+            flag=true;
+            break;
+        }
+    }
+   // print(arr);
+   // print(pre);
+    if(flag) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
