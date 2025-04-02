3d370072ddbda1239ae163b996467c7a8a929310
shivamkr022 <shivamshinu02@gmail.com>
2025-04-02 14:33:03 +0530

    cp

---
+++ codeforces/codeforces/1881D.cpp
@@ -0,0 +1,86 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+map<int,int>mp;
+void f(int x){
+    for(int i=2;i*i<=x;i++){  // prime fac
+        while(x%i==0){
+            mp[i]++;
+            x=x/i;
+        }
+    }
+    if(x>1){
+        mp[x]++;  // if x is prime
+    }
+}
+/* write core logic here */
+void solve(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    mp.clear();
+    for(int i=0;i<n;i++){
+        f(arr[i]);
+    }
+    bool flag  = true;
+    for(auto ele :mp){
+        if(ele.second %n !=0){
+            flag = false;
+            break;
+        }
+    }
+    if(flag) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
