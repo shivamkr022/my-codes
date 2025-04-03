70e01ad15ae7cba37a555f837c2fdb47e2b8dad7
shivamkr022 <shivamshinu02@gmail.com>
2025-04-03 13:38:10 +0530

    cp

---
+++ codeforces/codeforces/1931D.cpp
@@ -0,0 +1,71 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n,x,y;
+    cin>>n>>x>>y;
+    vector<int>arr(n);
+    for(auto & i:arr) cin>>i;
+    map<int, map<int, int>> mp;
+    int ans = 0;
+    for (auto & ele :arr) {
+        int v = (x - ele % x) % x;  // modular arithmetic proves   
+        int b = ele % y;   // modular arithmetic proves   
+        ans += mp[v][b];  
+        mp[ele % x][ele % y]++;
+    }
+    cout << ans << endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
