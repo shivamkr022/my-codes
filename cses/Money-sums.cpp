b135e3a69e307006656c9994ba2ed60bc6414c1b
shivamkr022 <shivamshinu02@gmail.com>
2025-04-11 18:18:33 +0530

    dp

--- cses/Money-sums.cpp
+++ cses/Money-sums.cpp
@@ -35,37 +35,17 @@
 #define mn(a,b,c) min(a,min(b,c))
 #define mx(a,b,c) max(a,max(b,c))
 using namespace std;
-int uniquePaths(vector<vector<int>>& obstacleGrid) {
-    int m = obstacleGrid.size();
-    int n = obstacleGrid[0].size();
-    vector<vector<int>>dp(m,vector<int>(n));
-    dp[0][0]=1;   // base case
-    if(obstacleGrid[m-1][n-1]!=0){
-        return 0;
-    }
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-        if (i == 0 && j == 0) continue; // <-- Important!
-            int up=0;
-            int dd=0;
-            if(i>0 and obstacleGrid[i-1][j]==0) up = dp[i-1][j];
-            if(j>0 and obstacleGrid[i][j-1]==0) dd = dp[i][j-1];
-            dp[i][j] = up+dd;
-        }
-    } 
-    return dp[m-1][n-1];
-}
+
 /* write core logic here */
 void solve(){
-    vector<vector<int>> arr;
-    int m = arr.size();
-    int n = arr[0].size();
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            cin>>arr[i][j];
-        }
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    set<int>s;
+    for(int i=0;i<n;i++){
+        
     }
-    cout<<uniquePaths(arr)<<endl;
 }
 /* logic ends */
 
