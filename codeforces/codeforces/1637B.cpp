a84d0566d394561a1c41d84c574a86afea05aeb2
shivamkr022 <shivamshinu02@gmail.com>
2025-04-12 19:31:09 +0530

    cp

---
+++ codeforces/codeforces/1637B.cpp
@@ -0,0 +1,78 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    string str;
+    cin >> str;
+    int n = str.length();
+    set<char> c;
+    bool flag = true;
+    int k;
+    for(k=0;k<n;k++){
+        if(c.find(str[k])==c.end())
+            c.insert(str[k]);
+        else
+            break;
+    }
+    for(int i=k;i<n;i++){
+        if(str[i]!=str[i-k])
+            flag = false;
+    }
+    if(flag)
+        cout << "YES" <<endl;
+    else
+        cout << "NO" <<endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
