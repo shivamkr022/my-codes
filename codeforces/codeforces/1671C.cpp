10dd8c144440b4608857fc48d86da508ec4f289f
shivamkr022 <shivamshinu02@gmail.com>
2025-04-28 12:37:16 +0530

    cp

---
+++ codeforces/codeforces/1671C.cpp
@@ -0,0 +1,82 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n;
+    long long x;
+    cin >> n >> x;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end());
+
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+        sum += a[i];
+    }
+
+    int prev = -1;
+    int ans = 0;
+    for (int i = n - 1; i >= 0; --i) {
+        int cur = (x - sum >= 0) ? (x - sum) / (i + 1) : -1;
+        ans += (i + 1) * (cur - prev);
+        prev = cur;
+        sum -= a[i];
+    }
+    cout << ans << endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
