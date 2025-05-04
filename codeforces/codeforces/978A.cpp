c86884fccc5914111f85d577f9b8b4a0592f43a3
shivamkr022 <shivamshinu02@gmail.com>
2025-05-04 19:23:13 +0530

    cp

---
+++ codeforces/codeforces/978A.cpp
@@ -0,0 +1,78 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> l(n);
+    for (int i = 0; i < n; i++) {
+        cin >> l[i];
+    }
+    stack<int> ret;
+    set<int> seen;
+    for (int i = n - 1; i >= 0; i--) {
+        if (seen.count(l[i]) == 0) {
+            seen.insert(l[i]);
+            ret.push(l[i]);
+        }
+    }
+    cout << ret.size() << endl;
+    while (!ret.empty()) {
+        cout << ret.top() << ' ';
+        ret.pop();
+    }
+    cout << endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    //cin>>t;
+    t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
