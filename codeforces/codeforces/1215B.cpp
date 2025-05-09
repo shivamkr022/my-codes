f0c6d8ab086473bdb225902cb61ffdc1abc351d4
shivamkr022 <shivamshinu02@gmail.com>
2025-05-09 19:56:51 +0530

    cp

---
+++ codeforces/codeforces/1215B.cpp
@@ -0,0 +1,72 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n;
+    cin >> n;
+    int pos = 1, neg = 0;
+    int sign = 1;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        if (x < 0) sign *= -1;
+        if (sign > 0) pos++;
+        else neg++;
+    }
+    int neg_cnt = pos * neg;
+    int pos_cnt = (pos * (pos - 1)) / 2 + (neg * (neg - 1)) / 2;
+    cout << neg_cnt << ' ' << pos_cnt <<endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    //cin>>t;
+    t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
