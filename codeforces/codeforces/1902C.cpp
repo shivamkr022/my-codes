f8ab52613801907249757be02a408c05a524fe8e
shivamkr022 <shivamshinu02@gmail.com>
2025-05-23 19:55:50 +0530

    cp

---
+++ codeforces/codeforces/1902C.cpp
@@ -0,0 +1,84 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+     int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    if (n == 1) {
+        cout << 1 << endl;
+        return ;
+    }
+    int g = 0;
+    for (int i = 1; i < n; i++) {
+        g = __gcd(g, abs(a[i] - a[0]));
+    }
+    sort(a.begin(), a.end(), greater<int>());
+    int ans = 0;
+    set<int> s(a.begin(), a.end());
+    int x = a[0];
+    while (s.count(x)) {
+        x -= g;
+    }
+    for (int i = 0; i < n; i++) {
+        ans += (a[0] - a[i]) / g;
+    }
+    ans += (a[0] - x) / g;
+    cout << ans << endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
