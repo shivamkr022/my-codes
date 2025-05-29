c943bb057cd3442938e7a197d805db7b89ab4cfd
shivamkr022 <shivamshinu02@gmail.com>
2025-05-29 14:39:51 +0530

    graph

--- codeforces/codeforces/263D.cpp
+++ codeforces/codeforces/263D.cpp
@@ -36,9 +36,54 @@
 #define mx(a,b,c) max(a,max(b,c))
 using namespace std;
 
+const int MAXN = 1e5 + 5;
+vector<int> adj[MAXN];
+int depth[MAXN], parent[MAXN];
+int n, m, k;
+bool found = false;
+
+void dfs(int u, int p, int d) {
+    depth[u] = d;
+    parent[u] = p;
+    for (int v : adj[u]) {
+        if (v == p) continue;
+        if (!depth[v]) {
+            dfs(v, u, d + 1);
+            if (found) return;
+        }
+        else if (depth[u] - depth[v] + 1 >= k + 1) {
+            vector<int> cycle;         // Cycle detected
+            int curr = u;
+            while (curr != v) {
+                cycle.push_back(curr);
+                curr = parent[curr];
+            }
+            cycle.push_back(v);
+            cout << cycle.size() << endl;
+            for (int node : cycle)
+                cout << node << ' ';
+            cout <<endl;
+            found = true;
+            return;
+        }
+    }
+}
+
 /* write core logic here */
 void solve(){
-    
+    cin >> n >> m >> k;
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);   // create adj list
+        adj[v].push_back(u);
+    }
+    for (int i = 1; i <= n; i++) {
+        if (!depth[i]) {
+            dfs(i, -1, 1);
+            if (found) break;
+        }
+    }
 }
 /* logic ends */
 
