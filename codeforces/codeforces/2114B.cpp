6ce6ccfb0fc18aac6a653bf361d7df4e324063a1
shivamkr022 <shivamshinu02@gmail.com>
2025-06-08 12:17:05 +0530

    cp

---
+++ codeforces/codeforces/2114B.cpp
@@ -0,0 +1,75 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+
+    int c0 = count(s.begin(),s.end(),'0');
+    int c1 = count(s.begin(),s.end(),'1');
+
+    int diff = abs(c0 - c1) / 2;
+    int tot = c0 / 2 + c1 / 2;
+
+    if(diff <= k && k <= tot && (diff % 2 == k % 2)) {
+        cout << "YES" <<endl;
+    }
+    else{
+        cout << "NO" <<endl;
+    }
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
