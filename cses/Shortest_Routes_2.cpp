c16a55aa10c3cb94facbd1caf4b8e21906a71152
shivamkr022 <shivamshinu02@gmail.com>
2025-06-29 14:37:58 +0530

    Cses

---
+++ cses/Shortest_Routes_2.cpp
@@ -0,0 +1,88 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam            |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+
+  Happy coding!
+*/
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+const int INF = 1e15;
+
+/* write core logic here */
+void solve(){
+    int n, m, q;
+    cin >> n >> m >> q;
+
+    vector<vector<int>> dist(n+1, vector<int>(n+1, INF));
+
+    // Distance to self = 0
+    for (int i = 1; i <= n; i++) dist[i][i] = 0;
+
+    // Read edges
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        dist[u][v] = min(dist[u][v], w); // if multiple edges
+        dist[v][u] = min(dist[v][u], w); // undirected
+    }
+
+    // Floyd-Warshall
+    for (int k = 1; k <= n; ++k) {
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (dist[i][k] < INF && dist[k][j] < INF)
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+
+    // Process queries
+    while (q--) {
+        int a, b;
+        cin >> a >> b;
+        if (dist[a][b] == INF) cout << -1 << endl;
+        else cout << dist[a][b] << endl;
+    }
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt", "w", stderr);
+    #endif
+    int t = 1;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
