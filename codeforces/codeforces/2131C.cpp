4dcb70dc0f0267ed1c379db9e5db8a5be598732a
shivamkr022 <shivamshinu02@gmail.com>
2025-08-15 02:09:19 +0530

    cp

---
+++ codeforces/codeforces/2131C.cpp
@@ -0,0 +1,79 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+      cin >> a[i];
+      a[i] %= k;
+      a[i] = min(a[i], k - a[i]);
+    }
+    vector<int> b(n);
+    for (int i = 0; i < n; i++) {
+      cin >> b[i];
+      b[i] %= k;
+      b[i] = min(b[i], k - b[i]);
+    }
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    if(a==b){
+        cout<<"YES"<<endl;
+        return ;
+    }
+    cout<<"NO"<<endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
