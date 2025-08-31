214dbba9773204aad950b15a8e305bca5da37bfa
shivamkr022 <shivamshinu02@gmail.com>
2025-09-01 02:12:46 +0530

    pythonnn

---
+++ Apython/Tupleee.py
@@ -0,0 +1,18 @@
+# tuple 
+
+# tup = (1,3,2,4,99,7,88)
+# print(tup)
+# print(type(tup))
+# print(tup[3:6])
+
+# p 1
+
+tup = ("c","d","a","a","b","b","a")
+
+lst = ["c","d","a","a","b","b","a"]
+
+lst.sort()
+print(lst)
+
+x = tup.count("a")
+print(x)
---
+++ Apython/list1.py
@@ -0,0 +1,12 @@
+# Print array
+
+# arr = [2,4,1,6,9,7]
+# print(arr)
+
+# Slicing of list
+
+arr = [3,1,8,5,6]
+lol = arr[2:4]
+print(lol)
+print(arr[2:5])
+print(arr)
