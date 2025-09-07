473f3d02b2c86da436f760e910b228ae6b76e3ce
shivamkr022 <shivamshinu02@gmail.com>
2025-09-08 01:08:22 +0530

    cp

---
+++ Contest/2137B.cpp
@@ -0,0 +1,82 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i] ;
+    }
+    if(n==2){
+        if(arr[0]==1){
+            cout<<2<<" "<<1<<endl;
+            return ;
+        }
+        else{
+            cout<<1<<" "<<2<<endl;
+            return ;    
+        }
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]==n){
+            cout<<n<<" ";
+        }
+        else{
+        cout<<n-arr[i]<<" ";
+        }
+    }cout<<endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+   // t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
