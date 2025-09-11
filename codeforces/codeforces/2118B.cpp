e79547e4c95d8f9326d35374c8a053a4fde77948
shivamkr022 <shivamshinu02@gmail.com>
2025-09-11 13:28:27 +0530

    cp

---
+++ codeforces/codeforces/2118B.cpp
@@ -0,0 +1,66 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n;
+    cin>>n;
+    cout<<(2*n-1)<<endl;
+    for(int i=1;i<n;i++){
+        cout<<i<<" "<<1<<" "<<i<<endl;
+        cout<<i<<" "<<i+1<<" "<<n<<endl;
+    }
+    cout<<n<<" "<<1<<" "<<n<<endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+   // t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
