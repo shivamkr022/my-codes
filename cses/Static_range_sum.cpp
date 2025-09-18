9ba9b03e7eb5ec0ed82264ec5563a1cb7351cc65
shivamkr022 <shivamshinu02@gmail.com>
2025-09-18 23:07:11 +0530

    CSES

---
+++ cses/Static_range_sum.cpp
@@ -0,0 +1,78 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n,q;
+    cin>>n>>q;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    vector<int>pre(n);
+    pre[0] = arr[0];
+    for(int i=1;i<n;i++){
+        pre[i] = pre[i-1]+arr[i];
+    }
+    //print(pre);
+    while(q--){
+        int a,b;
+        cin>>a>>b;
+        if(a>1){
+            cout<<abs(pre[a-2]-pre[b-1])<<endl;
+        }
+        else{
+            cout<<pre[b-1]<<endl;
+        }
+    }
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    //cin>>t;
+    t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
