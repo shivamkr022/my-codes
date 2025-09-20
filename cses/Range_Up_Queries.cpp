aa5fb59c685c6ddf4db5a951bdd9f97a3b72e196
shivamkr022 <shivamshinu02@gmail.com>
2025-09-20 06:52:19 +0530

    cses

---
+++ cses/Range_Up_Queries.cpp
@@ -0,0 +1,134 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Shivam     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \_/   /
+           \  -----  /
+             \_/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+struct node{
+    int sum;
+    node(){
+        sum = 0 ;
+    }
+};
+
+node merge(node a ,node b){
+    node ans ;
+    ans.sum = a.sum + b.sum ;
+    return ans ;
+}
+
+node tree[4*2000000]; // tree node ko array k form m store kar rhe hai
+
+void build(int idx ,int l ,int r){ // left and right k bich tree bna rhe hai
+
+    if(l==r){ // Leaf Node 
+        tree[idx].sum = 0; // mark every value as 0 init
+        return ;
+    }
+    int mid  = (l+r)/2;
+    build(2*idx,l,mid);
+    build(2*idx+1,mid+1,r);  // tree ban gya ab dono taraf se 
+    // ab merge hoga 
+    tree[idx] = merge(tree[2*idx], tree[2*idx+1]) ; 
+}
+
+void update (int idx ,int l ,int r , int pos , int val){ // position pe value ko update karna hai
+    if(pos < l or pos > r){
+        return ;
+    }
+    if(l==r){
+        tree[idx].sum+=val ; //Only the leaf node actually stores the original array value and gets updated directly.
+        return ;
+    }
+    int mid  = (l+r)/2;
+    update(2*idx,l,mid,pos ,val);
+    update(2*idx+1,mid+1,r,pos,val);  // tree ban gya ab dono taraf se 
+    // ab update hoga 
+    tree[idx] = merge(tree[2*idx], tree[2*idx+1]) ;  
+}
+
+node query(int idx ,int l ,int r ,int lq ,int rq){  // lq and lr are given query in ques 
+    if(lq > r or l>rq){
+        return node();
+    }
+    if(lq <= l && r <= rq){  // full overlap
+        return tree[idx];    
+    }
+    int mid = (l+r)/2;
+    return merge(query(2*idx,l,mid,lq,rq),query(2*idx+1,mid+1,r,lq,rq)) ;
+}
+
+/* write core logic here */
+void solve(){
+    int n,q;
+    cin>>n>>q;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    while(q--){
+        int ch;
+        cin>>ch;
+        if(ch==1){
+            int a,b,u;
+            cin>>a>>b>>u;
+            a--,b--;
+            update(1,0,n-1,a,u); // update karenge node 1 ko 0 se n-1 tak a aur b tk ko u se update
+            if(b+1 < n) update(1,0,n-1,b+1,-u);
+        }
+        else{
+            int k;
+            cin>>k;
+            k--;
+            node ans = query(1,0,n-1,0,k);
+            cout<< ans.sum + arr[k] <<endl ;
+        }
+    }
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    //cin>>t;
+    t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
